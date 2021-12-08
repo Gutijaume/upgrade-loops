a8859aaee43d288cc96115943293d3130097aeb1
gutijaume <gutijaume@hotmail.com>
2021-12-08 17:36:38 +0100

    Create iteracion#6.js
    Consigo el ejercicio mediante un fiilter e includes pero no entiendo el funcionamente del for of

---
+++ iteracion#6.js
@@ -0,0 +1,15 @@
+const toys = [
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
+
+let catToys = (data) => {
+  let filtered = data.filter((toy) => toy.name.includes("gato"));
+  console.log(filtered);
+};
+
+catToys(toys);
+
