fcaa49bf6b7a9e305cbc08d78911b4b431b06654
gutijaume <gutijaume@hotmail.com>
2021-12-08 16:48:37 +0100

    Create iteracion#5.js
    Creo que he buscado una formula demasiado sencilla ya que no estoy eliminando esos objetos. Simplemente estoy ocultándolos. Voy a profundizar en el ejercicio.

---
+++ iteracion#5.js
@@ -0,0 +1,24 @@
+const placesToTravel = [
+  { id: 5, name: "Japan" },
+  { id: 11, name: "Venecia" },
+  { id: 23, name: "Murcia" },
+  { id: 40, name: "Santander" },
+  { id: 44, name: "Filipinas" },
+  { id: 59, name: "Madagascar" },
+];
+
+let eliminator = (data) => {
+    
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id !== 11 && data[i].id !== 40) {
+      console.log (data[i])
+    }
+  }   
+};
+
+eliminator (placesToTravel)
+
+
+
+
+
