5d76845ee5e65172bdf2563924a98648ab0a9076
gutijaume <gutijaume@hotmail.com>
2021-12-08 12:09:05 +0100

    Update iteracion#3.js
    He dejado dos opciones: en la primera me imprimirá todos los valores convertidos en una cadena string y en el segundo podremos buscar el valor que queramos buscando por su posición en el array

--- iteracion#3.js
+++ iteracion#3.js
@@ -1,17 +1,27 @@
 const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
 
+// let cities = placesToTravel.join(" , ")
+
+// let listOfCities = (data) => {
+//     for(let value of data){
+//         value.placesToTravel
+//     }
+//     console.log(data[0])
+// }
+
+// listOfCities(cities)
 
 
-
-let cities = placesToTravel.join(" , ")
 
 let listOfCities = (data) => {
     for(let value of data){
         value.placesToTravel
     }
-    console.log(data)
+    console.log(data[2])
 }
 
-listOfCities(cities)
+listOfCities(placesToTravel)
+
+
 
 
